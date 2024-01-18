7d6a4507338bf27da70d5fbb2aa88a35d45e32dd
Igor <igorushakov111@gmail.com>
2024-01-18 21:00:21 +0700

    еще баго

--- dist/js/ImProfessional.js
+++ dist/js/ImProfessional.js
@@ -3,7 +3,8 @@ $('.removeMySkill').on('click', () => {
     $('.d-none').show()
 })
 
-$('.canelSkill').on('click', () => {
+$('#canelSkill').on('click', () => {
+    console.log("Нажимается");
     $('.mySkills').show()
     $('.d-none').hide()
 })
