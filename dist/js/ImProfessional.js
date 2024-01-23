365355f2a040d181b58266c8ddb33462ce174bac
Igor <igorushakov111@gmail.com>
2024-01-23 11:01:46 +0700

    Добавил опыт работы с change

--- dist/js/ImProfessional.js
+++ dist/js/ImProfessional.js
@@ -8,3 +8,14 @@ $('.canelSkill').on('click', () => {
     $('.mySkills').show()
     $('.d-none').hide()
 })
+$('.topInfoExpiriensItem').on('click', function() {
+    $(this).parent().find('.middleInfoExp').toggle();
+
+    $(this).find('.MyVacExp').toggleClass('boldEn');
+    $(this).find('.titleCompanyExp').toggleClass('boldEn');
+    $(this).find('.timeJobAndShowAll').find('.TimeJob').toggleClass('boldEn');
+
+
+    $(this).find('.timeJobAndShowAll').find('.expand').find('.rotatePls').toggleClass('rotate180');
+  });
+  
