39d90cec505424e539e8a566215bd28eff79932d
Igor <igorushakov111@gmail.com>
2024-11-13 12:13:25 +0700

    ПОиск

--- database/Request/User.js
+++ database/Request/User.js
@@ -1,47 +1,95 @@
 const UserSchema = require("../../database/Schema/UserSchema");
 
-const searchUserId = async(id) => {
-    try {
-        let result = await UserSchema.findOne({id})
-        return result
-    }
-    catch(e) {
-        return false
-    }
-}
+const searchUserId = async (id) => {
+  try {
+    let result = await UserSchema.findOne({ id });
+    return result;
+  } catch (e) {
+    return false;
+  }
+};
 
 const findUsersByFavorites = async (favorites) => {
-    try {
-      const personIds = favorites.map(fav => fav.person);
-  
-      const users = await UserSchema.find({id: personIds}).select('avatar job id name surname role'); // Предполагаем, что в вашей схеме есть поле id
+  try {
+    const personIds = favorites.map((fav) => fav.person);
 
-      // Если id является вашим собственным полем, его не нужно преобразовывать
-      const usersWithSelectedFields = users.map(user => ({
-        id: user.id,
-        avatar: user.avatar,
-        role: user.role,
-        job: user.job,
-        surname: user.surname,
-        name: user.name
-      }));
-  
-      return usersWithSelectedFields;
-    } catch (e) {
-      console.error("Ошибка при поиске пользователей:", e);
-      throw e; // или обрабатываем ошибку соответствующим образом
-    }
-  };
-  
+    const users = await UserSchema.find({ id: personIds }).select(
+      "avatar job id name surname role"
+    ); // Предполагаем, что в вашей схеме есть поле id
 
-const searchUserEmail = async(email) => {
-    try {
-        let result = await UserSchema.findOne({mail:email})
-        return result
-    }
-    catch(e) {
-        return false
-    }
-}
+    // Если id является вашим собственным полем, его не нужно преобразовывать
+    const usersWithSelectedFields = users.map((user) => ({
+      id: user.id,
+      avatar: user.avatar,
+      role: user.role,
+      job: user.job,
+      surname: user.surname,
+      name: user.name,
+    }));
 
-module.exports = {searchUserId,searchUserEmail,findUsersByFavorites}
+    return usersWithSelectedFields;
+  } catch (e) {
+    console.error("Ошибка при поиске пользователей:", e);
+    throw e; // или обрабатываем ошибку соответствующим образом
+  }
+};
+
+let getSpecialList = async (data) => {
+  try {
+    for (const [key, value] of Object.entries(data)) {
+      if (!value === null) {
+        delete data[key];
+      } else if (typeof value === "string") {
+        data[key] = new RegExp(`.*${value}.*`, "i");
+      } else if (typeof value === "object") {
+        data[key] = {
+          $elemMatch: {
+            title: { $in: value.map((v) => new RegExp(`.*${v}.*`, "i")) },
+          },
+        };
+      }
+    }
+    const users = await UserSchema.find({ role: "worker", ...data }).select(
+      "avatar job id name surname role skills description city expiriens"
+    );
+    console.log(
+      users[0].expiriens.reduce((acc, item) => {
+        let month = 0;
+        let year = 0;
+
+        if (item.typeData === "m") {
+          month = +Number(item.date);
+          // Если месяцев больше 12, переводим в годы
+          year = Math.floor(month / 12); // Конвертируем месяцы в годы
+          month = month % 12; // Остаток месяцев
+        } else if (item.typeData === "y") {
+          year = +Number(item.date);
+        }
+
+        // Суммируем года и добавляем в аккумулятор
+        return acc + year + month / 12; // Месяцы прибавляются как доля года
+      }, 0)
+    );
+    console.log(users[0].expiriens);
+    return { success: true, users };
+  } catch (e) {
+    console.log(e.message);
+    return { success: false, message: "Ошибка при получении пользователей" };
+  }
+};
+
+const searchUserEmail = async (email) => {
+  try {
+    let result = await UserSchema.findOne({ mail: email });
+    return result;
+  } catch (e) {
+    return false;
+  }
+};
+
+module.exports = {
+  searchUserId,
+  searchUserEmail,
+  findUsersByFavorites,
+  getSpecialList,
+};
--- pageRoutes/specialist.js
+++ pageRoutes/specialist.js
@@ -1,13 +1,27 @@
-const {Router} = require('express');
-const router = Router()
-const {decodeAccessToken} = require('../api/tokens/accessToken')
-const {isAuthNotRequire} = require('../api/middlewares/authNotRequire')
+const { Router } = require("express");
+const router = Router();
+const { decodeAccessToken } = require("../api/tokens/accessToken");
+const { isAuthNotRequire } = require("../api/middlewares/authNotRequire");
+const { getSpecialList } = require("../database/Request/User");
 
-router.get('/specialists', isAuthNotRequire,(req,res) => {    
-    let access = req.cookies.access;
-    let user = decodeAccessToken(access)
-    console.log(user)
-      res.render('specialist', { isLoggedIn:!!user, id:user.userID,chatList: user.chatList || null})
-})
+router.get("/specialists", isAuthNotRequire, async (req, res) => {
+  let access = req.cookies.access;
+  let user = await decodeAccessToken(access);
+  let userData = {
+    name: "иго",
+    surname: "уша",
+    job: "frontEnd",
+    city: "АРЗАМАС",
+    skills: ["Node.js", "121212 б"],
+    // expiriens: []
+  };
 
-module.exports = router
+  let users = await getSpecialList(userData);
+  res.render("specialist", {
+    isLoggedIn: !!user,
+    id: user.userID,
+    chatList: user.chatList || null,
+  });
+});
+
+module.exports = router;
