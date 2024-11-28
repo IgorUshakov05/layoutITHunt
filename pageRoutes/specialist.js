1124fdac0da4f4e3e74f75d3891f58d562b768c1
Igor <igorushakov111@gmail.com>
2024-11-28 09:17:02 +0700

    Footer link

--- pageRoutes/specialist.js
+++ pageRoutes/specialist.js
@@ -5,24 +5,29 @@ const { isAuthNotRequire } = require("../api/middlewares/authNotRequire");
 const { getSpecialList } = require("../database/Request/User");
 
 router.get("/specialists", isAuthNotRequire, async (req, res) => {
-  let access = req.cookies.access;
-  let user = await decodeAccessToken(access);
-  let userData = {
-    job: req.query?.job ? JSON.parse(req?.query?.job) : null,
-    name: req.query.name || null,
-    surname: req.query.surname || null,
-    skills: req.query?.skills ? JSON.parse(req?.query?.skills) : null,
-    city: req.query.city || null,
-  };
-  console.log(userData);
-  let users = await getSpecialList(userData, user?.userID);
-  if (!users.success) return res.render("404");
-  res.render("specialist", {
-    isLoggedIn: !!user,
-    id: user.userID,
-    users: users.users,
-    chatList: user.chatList || null,
-  });
+  try {
+    let access = req.cookies.access;
+    let user = await decodeAccessToken(access);
+    let userData = {
+      job: req.query?.job ? JSON.parse(req?.query?.job) : null,
+      name: req.query.name || null,
+      surname: req.query.surname || null,
+      skills: req.query?.skills ? JSON.parse(req?.query?.skills) : null,
+      city: req.query.city || null,
+    };
+    console.log(userData);
+    let users = await getSpecialList(userData, user?.userID);
+    if (!users.success) return res.render("404");
+    res.render("specialist", {
+      isLoggedIn: !!user,
+      id: user.userID,
+      users: users.users,
+      chatList: user.chatList || null,
+    });
+  } catch (e) {
+    console.log(e)
+    return res.redirect("501");
+  }
 });
 
 module.exports = router;
